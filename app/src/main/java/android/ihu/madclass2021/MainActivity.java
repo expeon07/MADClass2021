138a278f2d35db7b859e11aa5827a9fa8194f733
Sheena Fernandez <sheena.fernandez78@gmail.com>
2021-04-15 01:27:12 +0300

    Write condition statements.

--- app/src/main/java/android/ihu/madclass2021/MainActivity.java
+++ app/src/main/java/android/ihu/madclass2021/MainActivity.java
@@ -4,11 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    static final String TOKEN_VERIFICATION_URL = "http://mad.mywork.gr/authenticate.php?t=XYZ";
+    String token = "";
+    static final String TOKEN_VERIFICATION_URL = "http://mad.mywork.gr/authenticate.php?t=";
     static final String EMAIL_VERIFICATION_URL = "http://mad.mywork.gr/generate_token.php?e=";
 
     AuthenticationTask authenticationTask = new AuthenticationTask();
@@ -23,14 +29,14 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
-        // contact and download the contents of the Web page located
+        // TODO contact and download the contents of the Web page located
         // at http://mad.mywork.gr/authenticate.php?t=XYZ
         String response = "";
-        authenticationTask.execute(TOKEN_VERIFICATION_URL, response); // .get();
+        authenticationTask.execute(TOKEN_VERIFICATION_URL + token, response); // .get();
 
         Log.i("Response", response);
-        String status = "";
-        String message = "";
+        String status = "0-FAIL"; //"";
+        String message = "hiyaaaa"; //"";
 
         verifyToken(status, message);
 
@@ -39,9 +45,12 @@ public class MainActivity extends AppCompatActivity {
 
     private void verifyToken(String status, String message) {
         if (status.equals("0-OK")) {
-            // print message on screen
+            setContentView(R.layout.menu_activity);
+
+            TextView menu_greeting = findViewById(R.id.menu_greeting);
+            menu_greeting.setText(message);
+
         } else if (status.equals("0-FAIL")) {
-            // call obtainToken method
             obtainToken();
         }
     }
@@ -49,29 +58,37 @@ public class MainActivity extends AppCompatActivity {
     private void obtainToken() {
         setContentView(R.layout.login_activity);
 
-        // listen to submit button
-        // get email
+        ImageButton submit_email = findViewById(R.id.submit_email);
+        submit_email.setOnClickListener(v -> {
+            EditText email =  findViewById(R.id.email_address);
+            String user_email = email.getText().toString();
 
-        // contact another Web page,
-        // located at http://mad.mywork.gr/generate_token.php?e=your_email
-        String user_email = "";
-        String status = "";
+            // TODO contact another Web page,
+            // located at http://mad.mywork.gr/generate_token.php?e=your_email
+            String status = "1-OK"; //"";
+
+            verifyEmail(status, user_email);
+        });
+
     }
 
     private void verifyEmail(String status, String user_email) {
         if (status.equals("1-OK")) {
-            // make everything invisible and
-            // print on the screen what it is sent inside the msg tag
-
-            // Rerun the application.
-            // You should be immediately transferred to MenuActivity
-            setContentView(R.layout.menu_activity);
+            // TODO Rerun the application and set new token
+            //setContentView(R.layout.menu_activity);
 
         } else if (status.equals("1-FAIL")) {
             // print error message(make visible):
-            // "Cannot identify email user_email"
+            String error_msg = getResources().getString(R.string.email_error, user_email);
+
+            TextView login_error = findViewById(R.id.email_error);
+            login_error.setText(error_msg);
+            login_error.setVisibility(View.VISIBLE);
 
             // Toast: Authentication Failed
+            Toast.makeText(getApplicationContext(),
+                    "Authentication Failed",
+                    Toast.LENGTH_SHORT).show();
         }
-     }
+    }
 }
