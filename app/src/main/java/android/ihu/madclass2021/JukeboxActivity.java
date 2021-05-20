f76678ab593c515a72873275161ab26dd17e248a
Sheena Fernandez <sheena.fernandez78@gmail.com>
2021-05-20 09:32:04 +0300

    Add JukeboxActivity

---
+++ app/src/main/java/android/ihu/madclass2021/JukeboxActivity.java
@@ -0,0 +1,14 @@
+package android.ihu.madclass2021;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class JukeboxActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_jukebox);
+    }
+}
